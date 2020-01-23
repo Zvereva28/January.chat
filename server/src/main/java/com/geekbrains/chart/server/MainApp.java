8691c376fcdc30cabdf8b212c438f11c7f531a01
Zvereva28 <keg28@ya.ru>
2020-01-23 13:31:53 +0400

    Home Work, lesson 6

---
+++ server/src/main/java/com/geekbrains/chart/server/MainApp.java
@@ -0,0 +1,31 @@
+package com.geekbrains.chart.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainApp {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8989)){
+            System.out.println("Сервер запущен. Ожидаем подключения клиентов...");
+            Socket socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            while (true) {
+                String msg = in.readUTF();
+                if (!msg.equals("/end")){
+                System.out.print("Сообщение от клиента: " + msg + "\n");
+                out.writeUTF("echo: " + msg);}
+                else {
+                    out.writeUTF(msg);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
